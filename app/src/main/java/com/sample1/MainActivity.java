f6d3221762bc2bb17f0220b6ce616e19c2e3256a
Praveen Chowdary Pasumarthi <praven.pasumarthi@gmail.com>
2018-02-08 14:08:03 +0530

    message

---
+++ app/src/main/java/com/sample1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sample1;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/sample1/NewMessageActivity.java
@@ -0,0 +1,8 @@
+package com.sample1;
+
+/**
+ * Created by chowd on 08-02-2018.
+ */
+
+class NewMessageActivity {
+}
